00c1e73a69e9ff4e01e2af67a530f0f69d4a268e
iamayoda <yandi.mtati@gmail.com>
2025-05-26 14:33:20 +0200

    Create Task 4: Variables.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 001/Task 4: Variables.py
@@ -0,0 +1,13 @@
+input("What is your name?")
+
+#1. Use variable to store input
+name = input("What is your name?")
+print(name)
+
+#2. Check the length of the user input
+print(len(name))
+
+#3. Use "username and "length" variable get print(length).
+username = input("What is your name?")
+length = len(username)
+print(length)

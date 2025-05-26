0dbc2bf12b21fc7a8b195c47adbfafb81f7eba3c
iamayoda <yandi.mtati@gmail.com>
2025-05-26 14:34:17 +0200

    Create Task 5: Variable Naming.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 001/Task 5: Variable Naming.py
@@ -0,0 +1,13 @@
+#1. Default variable
+name = "Angela"
+length = len(name)
+print(length)
+
+#2. Alternative variable 1
+fav_movie = input("What is your favourite movie? ")
+print(fav_movie)
+
+#3. Alternative variable 2
+name = "Ayanda"
+surname = "Mtati"
+print("My full name is " + name + " " + surname)

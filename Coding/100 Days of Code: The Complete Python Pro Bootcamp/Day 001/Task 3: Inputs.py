fceada74d468b1e2cbe37b5d3d6486e368e28df9
iamayoda <yandi.mtati@gmail.com>
2025-05-26 14:31:28 +0200

    Create Task 3: Inputs.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 001/Task 3: Inputs.py
@@ -0,0 +1,8 @@
+#1. input = prompt for the user
+input("What is your name? ")
+
+#2. Printing input
+print("Hello " + input("What is your name? "))
+
+#3. Add Exclamation Mark
+print("Hello " + input("What is your name? ") + "!")

5920d324107ba18a67b0dad97def05a1b319f12f
iamayoda <yandi.mtati@gmail.com>
2025-06-24 21:21:52 +0200

    Create Task 01: Data Types.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 002/Task 01: Data Types.py
@@ -0,0 +1,20 @@
+#1. SUBSCRIPTING
+
+print("Hello"[0]) #Position of the first character - start from 0
+
+print("Hello"[4]) #Position of the last character - start from 0
+
+print("Hello"[-1]) #Negative indices - "-1" is the last character
+
+print("Hello"[-5]) #Negative indices - "-1" is the last character
+
+
+#2. OTHER DATA TYPES
+
+print("123" + "456") #"" = String even for numbers (integers) thus the following will be concatenation
+
+print(123 + 456) #Integer - Whole number without ""
+
+print(3.14159) #Float - Floating point number (decimal)
+
+True or False #Boolean

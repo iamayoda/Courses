73953bdda42fd4dbbbd657e0511790d5869afc99
iamayoda <yandi.mtati@gmail.com>
2025-06-24 22:00:09 +0200

    Create Task 01: For Loops.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 005/Task 01: For Loops.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Peach", "Pear"]
+
+#1 LOOPS - Allow us to tell the computer to repeat actions without having to write repeated code.
+for fruit in fruits:
+    print(fruit)
+    print(fruit +" pie")
+
+print(fruits)

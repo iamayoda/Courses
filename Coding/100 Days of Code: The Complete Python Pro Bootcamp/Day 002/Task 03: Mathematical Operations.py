fed9cbe0e7b95d641d9c06ad6d71425ed3f13337
iamayoda <yandi.mtati@gmail.com>
2025-06-24 21:23:43 +0200

    Create Task 03: Mathematical Operations.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 002/Task 03: Mathematical Operations.py
@@ -0,0 +1,22 @@
+print("My age: " + str(12))
+
+#1 Basic Operators
+print(12 + 34) #Addition
+print(34 - 12) #Subtraction
+print(12 * 34) #Multiplication
+print(34 / 12) #Division
+
+#2. Implicit typecast which is default in python
+print(34 / 12) #float
+print(34 // 12) #Integer - removes decimal values
+print(8**3) #Exponent - power of
+
+#3. BODMAS/BOMDAS/PEMDAS
+# ()
+# **
+# * OR /
+# + OR -
+
+#Example
+print(3 * 3 + 3 / 3 - 3) # =7
+print(3 * (3 + 3) / 3 - 3) # =3

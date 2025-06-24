cd97126b0c473dd864ce9b42fb2ab9cfc7bfd232
iamayoda <yandi.mtati@gmail.com>
2025-06-24 21:32:48 +0200

    Create Task 01: If Else.py

---
+++ Coding/100 Days of Code: The Complete Python Pro Bootcamp/Day 003/Task 01: If Else.py
@@ -0,0 +1,20 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+#1. If Else
+if height >= 120: # If condition is true.
+    print("You can ride the rollercoaster.")
+else: # If condition is false.
+    print("Sorry you have to grow taller before you can ride.")
+
+#2. Comparator Operators
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+
+#3. Equal to (=) sign
+# "=" Assigns a value to a variable.
+# "==" Checks Equality between two value.
